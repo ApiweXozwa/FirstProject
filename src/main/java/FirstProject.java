0299e86df0400eee44d16dd459d3c439689f6a68
LukhanyoThando <thandonko141@gmail.com>
2020-02-19 01:23:31 +0200

    This project calculates the sum of two integers

---
+++ src/main/java/FirstProject.java
@@ -0,0 +1,7 @@
+public class FirstProject {
+    public static void main(String[] args) {
+        int sum, one = 1, two = 2;
+        sum = one + two;
+        System.out.println("The sum is"+" "+ sum);
+    }
+}
